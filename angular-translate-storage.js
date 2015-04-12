d9ba6afdc001afabba67feb0229a6247667e66ea
tsaikd <tsaikd@gmail.com>
2015-04-12 21:54:52 +0800

    add compiled js
    Signed-off-by: tsaikd <tsaikd@gmail.com>

---
+++ angular-translate-storage.js
@@ -0,0 +1,29 @@
+/* angular-translate-storage v0.0.1 2015-04-12
+ * https://github.com/tsaikd/angular-translate-storage
+ * License: MIT
+ */
+
+angular
+
+.module("angular-translate-storage", [
+	"LocalStorageModule"
+])
+
+.factory("translateStorage"
+	, [       "localStorageService"
+	, function(localStorageService) {
+
+	function TranslateStorage() {}
+
+	TranslateStorage.prototype.get = function() {
+		return localStorageService.get.apply(this, arguments);
+	};
+
+	TranslateStorage.prototype.put = function() {
+		return localStorageService.set.apply(this, arguments);
+	};
+
+	return new TranslateStorage();
+}])
+
+;
